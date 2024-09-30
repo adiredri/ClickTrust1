bb1138c7aab318a4eedf508919520aa2de9c182b
Adir Edri <adire7399@gmail.com>
2024-09-30 20:06:02 +0300

    UpdateConnectMongo

---
+++ backend/config/routePages.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const User = require('../models/user');
+const Asset = require('../models/asset');
+const Trade = require('../models/trades');
+const Contact = require('../models/contact');
+
+
+
+
+
+
+
+
+module.exports = router;
